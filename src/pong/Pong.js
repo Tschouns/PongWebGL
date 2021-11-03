e15a4f658948c6a2552d2992e0c4b0956d014bfb
Jonas Aklin <jonas.aklin@faigle.ch>
2021-11-03 23:03:07 +0100

    cleaned up Pong.js (based on "pong_start")

--- src/pong/Pong.js
+++ src/pong/Pong.js
@@ -15,10 +15,8 @@ var ctx = {
     shaderProgram: -1,
 
     aVertexPositionId: -1,
-    uProjectionMatId: -1,
+    uProjectionMatrixId: -1,
     uColorId: -1,
-
-    positionBuffer: -1,
 };
 
 // we keep all the parameters for drawing a specific object together
@@ -37,10 +35,6 @@ function startup() {
     window.addEventListener('keyup', onKeyup, false);
     window.addEventListener('keydown', onKeydown, false);
     draw();
-    //var loop1 = [0.1, 0.1, 0.9, 0.9, 0.1, 0.9];
-    //var loop2 = [0.2, 0.2, 0.8, 0.8, 0.2, 0.8];
-    //drawPoints(loop1, gl.LINE_LOOP);
-    //drawPoints(loop2, gl.LINE_LOOP);
 }
 
 /**
@@ -61,7 +55,7 @@ function initGL() {
 function setUpAttributesAndUniforms(){
     "use strict";
     ctx.aVertexPositionId = gl.getAttribLocation(ctx.shaderProgram, "aVertexPosition");
-    ctx.uProjectionMatId = gl.getUniformLocation(ctx.shaderProgram, "uProjectionMat");
+    ctx.uProjectionMatrixId = gl.getUniformLocation(ctx.shaderProgram, "uProjectionMatrix");
     ctx.uColorId = gl.getUniformLocation(ctx.shaderProgram, "uColor");
 }
 
@@ -70,32 +64,14 @@ function setUpAttributesAndUniforms(){
  */
 function setUpBuffers(){
     "use strict";
-    ctx.positionBuffer = gl.createBuffer();
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, ctx.positionBuffer);
-    gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(ctx.aVertexPositionId);
-
     rectangleObject.buffer = gl.createBuffer();
     var vertices = [
         -0.5, -0.5,
         0.5, -0.5,
-        -0.5, 0.5,
-        0.5, 0.5];
-
+        0.5, 0.5,
+        -0.5, 0.5];
     gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-}
-
-function drawPoints(pointCoordinates, mode) {
-    "use strict";
-    console.log("Drawing shape...");
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(pointCoordinates), gl.STATIC_DRAW);
-
-    gl.uniform4f(ctx.uColorId, 1, 1, 1, 1);
-    gl.drawArrays(mode, 0, pointCoordinates.length / 2);
 }
 
 /**
