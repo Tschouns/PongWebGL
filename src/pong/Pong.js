af424947d6db8366eaef0e354974b99666006365
Jonas Aklin <jonas.aklin@faigle.ch>
2021-11-04 12:46:05 +0100

    line and paddles are drawn

--- src/pong/Pong.js
+++ src/pong/Pong.js
@@ -57,7 +57,7 @@ function initGL() {
     setUpAttributesAndUniforms();
     setUpBuffers();
     
-    gl.clearColor(0.2, 0.2, 0.2, 1);
+    gl.clearColor(0.2, 0.25, 0.2, 1);
 }
 
 /**
@@ -93,8 +93,7 @@ function setUpBuffers(){
  */
 function drawRectangle(prjectionMatrix, colorRgb) {
     "use strict";
-    console.log("Drawing");
-    //gl.clear(gl.COLOR_BUFFER_BIT);
+    console.log("Drawing rectangle...");
 
     gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
     gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 0, 0);
@@ -141,22 +140,15 @@ function updatePongWorld() {
  * Draws the entire "Pong World".
  */
 function drawPongWorld() {
+    "use strict";
+
     // Clear screen.
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Draw middle line.
-    // var middleLineTransform = mat3.copy(mat3.create(), pongWorld.middleLine.modelMatrix);
-    // mat3.translate(middleLineTransform, middleLineTransform, pongWorld.middleLine.position);
-
-    var middleLineMatrix = mat3.copy(mat3.create(), pongWorld.middleLine.modelMatrix);
-    var moveToPosition = mat3.fromTranslation(mat3.create(), pongWorld.middleLine.position);
-
-    mat3.multiply(middleLineMatrix, moveToPosition, middleLineMatrix);
-
-    //mat3.multiply(middleLineTransform, pongWorld.middleLine.modelMatrix, middleLineTransform);
-    //mat3.multiply(middleLineTransform)
-
-    drawRectangle(middleLineMatrix, pongWorld.middleLine.color);
+    // Draw the game models.
+    drawGameModel(pongWorld.middleLine);
+    drawGameModel(pongWorld.paddleLeft);
+    drawGameModel(pongWorld.paddleRight);
 
     // Demo 1.
     var scale = mat3.fromScaling(mat3.create(), vec2.fromValues(40, 100));
@@ -169,7 +161,7 @@ function drawPongWorld() {
     mat3.multiply(transform1, rotation, transform1);
     mat3.multiply(transform1, translation1, transform1);
 
-    drawRectangle(transform1, vec4.fromValues(1,1,1,1));
+    //drawRectangle(transform1, vec4.fromValues(1,1,1,1));
 
     // Demo 2.
     var transform2 = mat3.create();
@@ -177,18 +169,61 @@ function drawPongWorld() {
     mat3.multiply(transform2, rotation, transform2);
     mat3.multiply(transform2, translation2, transform2);
 
-    drawRectangle(transform2, vec4.fromValues(0,1,0,1));
+    //drawRectangle(transform2, vec4.fromValues(0,1,0,1));
+}
+
+function drawGameModel(model) {
+    "use strict";
+    console.log("Drawing model...");
+
+    var modelMatrix = mat3.copy(mat3.create(), model.modelMatrix);
+
+    var moveToPosition = mat3.fromTranslation(mat3.create(), model.position);
+    mat3.multiply(modelMatrix, moveToPosition, modelMatrix);
+
+    drawRectangle(modelMatrix, model.color);
 }
 
 /**
  * Initializes the "Pong World".
  */
 function initializePongWorld() {
-    pongWorld.middleLine = {
-        modelMatrix: createGameModelMatrix(vec2.fromValues(2, 560)),
-        position: vec2.fromValues(400, 300),
-        color: vec4.fromValues(1, 1, 1, 1),
-    };
+    "use strict";
+
+    pongWorld.middleLine = createGameModel(
+        vec2.fromValues(2, 560),
+        vec4.fromValues(1, 1, 1, 0.9), // light gray
+        vec2.fromValues(400, 300));
+
+    pongWorld.paddleLeft = createGameModel(
+        vec2.fromValues(10, 100),
+        vec4.fromValues(1, 1, 1, 1),
+        vec2.fromValues(100, 300));
+
+    pongWorld.paddleRight = createGameModel(
+        vec2.fromValues(10, 100),
+        vec4.fromValues(1, 1, 1, 1),
+        vec2.fromValues(700, 300));
+}
+
+/**
+ * Initializes a drawable "game model" which has a shape (model matrix), a color, and a position (which might change during the game).
+ * @param {vec2} size the model's size
+ * @param {vec4} color the model's color
+ * @param {vec2} startingPosition the model's starting position
+ * @returns {{color: *, modelMatrix: mat3, position: *}}
+ */
+function createGameModel(size, color, startingPosition) {
+    "use strict";
+
+    var scaleToSize = mat3.fromScaling(mat3.create(), size);
+    var model = {
+        modelMatrix: scaleToSize,
+        color: color,
+        position: startingPosition,
+    }
+
+    return model;
 }
 
 /**
@@ -198,6 +233,8 @@ function initializePongWorld() {
  * @returns {mat3} the model matrix
  */
 function createGameModelMatrix(size) {
+    "use strict";
+
     var scaleToSize = mat3.fromScaling(mat3.create(), size);
 
     return scaleToSize;
